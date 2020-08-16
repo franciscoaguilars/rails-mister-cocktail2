fd0d6b35fee6fc067682dfe5f94b605a7e399d9a
Francisco Aguilar <fiaguilars@gmail.com>
2020-08-16 17:51:32 -0500

    Added Models/ DB schema

---
+++ db/migrate/20200816223334_create_cocktails.rb
@@ -0,0 +1,10 @@
+class CreateCocktails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cocktails do |t|
+      t.string :name
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
